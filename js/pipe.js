1303915a1f7a247efb4e99864523d618dbfded8a
Le Thi My Tien <tienltm@icts.vn>
2019-09-26 18:43:45 +0700

    create pipe

---
+++ js/pipe.js
@@ -0,0 +1,28 @@
+export default class Pipe {
+    width = 100;
+    height = 70;
+    top = 0;
+    left = 0;
+    isTopPipe = false;
+    constructor(isTopPipe) {
+        this.isTopPipe = isTopPipe;
+        this.pipe = document.createElement('div');
+    }
+
+    getDOM() {
+        return this.pipe;
+    }
+    update() {
+        this.pipe.style.backgroundImage = 'url(assets/sprites/pipe-green.png)';
+        this.pipe.style.width = this.width + 'px';
+        this.pipe.style.height = this.height + 'px';
+        this.pipe.style.top = this.top + 'px';
+        this.pipe.style.left = this.left + 'px';
+        this.pipe.style.position = 'absolute';
+        this.pipe.style.backgroundRepeat = 'no-repeat';
+        this.pipe.style.backgroundSize = 'cover';
+        if (this.isTopPipe) {
+            this.pipe.style.transform = 'rotate(180deg)';
+        }
+    }
+}
--- js/scenery.js
+++ js/scenery.js
@@ -1,19 +1,20 @@
 import Ground from "./ground.js";
 import Sky from "./sky.js";
 import Bird from "./bird.js";
+import Pipe from "./pipe.js";
 
 export default class Scenery {
-    constructor (width, height) {
+    constructor(width, height) {
         this.width = width;
         this.height = height;
         this.scenery = document.getElementById("scenery");
-        
+
         this.ground = new Ground();
         this.ground.width = this.width;
         this.ground.height = 80;
         this.ground.top = this.height - 80;
         this.scenery.appendChild(this.ground.getDOM());
-        
+
         this.sky = new Sky();
         this.sky.width = this.width;
         this.sky.height = this.height - 80;
@@ -25,14 +26,32 @@ export default class Scenery {
         window.onclick = () => {
             this.bird.flyUp();
         }
+        this.pipes = [];
+        for (let i = 0; i < 6; i++) {
+            const distance = 150;
+            const pipeHeight = 50 + ~~(Math.random() * 200);
+            const pipeTop = new Pipe(true);
+            pipeTop.height = pipeHeight;
+            pipeTop.left = (i + 1) * 200;
+            this.scenery.appendChild(pipeTop.getDOM());
+
+            const pipeBottom = new Pipe(false);
+            pipeBottom.height = this.height - this.ground.height - pipeHeight - distance;
+            pipeBottom.top = pipeHeight + distance;
+            pipeBottom.left = (i + 1) * 200;
+            this.scenery.appendChild(pipeBottom.getDOM());
+            this.pipes.push(pipeTop, pipeBottom);
+        }
+
     }
-    
+
     update() {
-        if(this.bird.top  + this.bird.height > this.ground.top  ) {
+        if (this.bird.top + this.bird.height > this.ground.top) {
             this.bird.flyUp();
         }
         this.ground.update();
         this.bird.update();
         this.sky.update();
+        this.pipes.forEach(pipe => pipe.update());
     }
 }
